eb99390aaa07a64a6cfd7ea3dbc3b2ff4a051c41
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-03-24 02:19:39 +0100

    274. H-Index

---
+++ LetCode/274. H-Index/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse=True)
+        print(citations)
+        for i, c in enumerate(citations):
+            if c < i+1:
+                return i
+        
+        return len(citations)
+        
