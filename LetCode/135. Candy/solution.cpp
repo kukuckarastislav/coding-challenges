403f55492f0a7b30d501ad18613179cce8ee87c6
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-03-27 03:51:49 +0100

    LeetCode: 135. Candy

---
+++ LetCode/135. Candy/solution.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        for(int i = 1; i < n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1] + 1;
+            }
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            if(ratings[i] > ratings[i+1]){
+                if(candies[i] <= candies[i+1]){
+                    candies[i] = candies[i+1] + 1;
+                }
+            }
+        }
+
+        int minCandies = 0;
+        for(int c : candies){
+            minCandies += c;
+        }
+
+        return minCandies;
+    }
+};
