61e844b10e7b81f81243e2a4445a3ea3a84fd402
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-04-01 18:45:56 +0200

    LeetCode: 13. Roman to Integer

---
+++ LetCode/13. Roman to Integer/solution.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int n = s.size();
+        int result = 0;
+        int prevRomanChar = 5000;
+        
+        int i = 0;
+        while(i < n){
+            int current = romanCharToInt(s[i]);
+            if(prevRomanChar < current) current -= prevRomanChar*2;
+            prevRomanChar = current;
+            result += current;
+
+            i++;
+        }
+
+        return result;
+    }
+
+    int romanCharToInt(char c){
+        switch(c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return -1;
+        }
+        return -1;
+    }
+};
