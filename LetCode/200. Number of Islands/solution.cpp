ec98a14887880199c5eed4d8447c4cef5aa89298
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-04-19 03:09:35 +0200

    LeetCode: 200. Number of Islands

---
+++ LetCode/200. Number of Islands/solution.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int islands = 0;
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == '1'){
+                    sinkIslands(grid, i,j);
+                    islands++;
+                }
+            }
+        }
+
+        return islands;
+    }
+
+    void sinkIslands(vector<vector<char>>& grid, int i, int j){
+
+        grid[i][j] = '0';
+
+        if(i-1 >= 0 && grid[i-1][j] == '1') 
+            sinkIslands(grid, i-1, j);
+        
+        if(i+1 < grid.size() && grid[i+1][j] == '1') 
+            sinkIslands(grid, i+1, j);
+        
+        if(j-1 >= 0 && grid[i][j-1] == '1')
+            sinkIslands(grid, i, j-1);
+
+        if(j+1 < grid[0].size() && grid[i][j+1] == '1')
+            sinkIslands(grid, i, j+1);
+    }
+};
