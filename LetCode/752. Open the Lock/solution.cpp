5d507fbd2901064eae3b434c933d17fac34eeec7
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-04-22 15:56:38 +0200

    .gitignore is now working

---
+++ LetCode/752. Open the Lock/solution.cpp
@@ -0,0 +1,28 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+
+
+
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        int rasti = 3;
+        return 0;
+    }
+};
+
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+
+    
+    cout << endl;
+}
