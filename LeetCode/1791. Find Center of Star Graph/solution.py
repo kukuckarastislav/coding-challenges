e9cd5b84420e9e53bce910fd9354477b240b1365
kukuckarastislav <73312345+kukuckarastislav@users.noreply.github.com>
2024-06-27 02:22:48 +0200

    LeetCode: 1791. Find Center of Star Graph

---
+++ LeetCode/1791. Find Center of Star Graph/solution.py
@@ -0,0 +1,20 @@
+from types import List
+from collections import defaultdict
+
+# Runtime 642ms Beats 88.21%
+# Memory 50.87MB Beats 99.81%
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        mp = defaultdict(int)
+
+        for e1, e2 in edges:
+            mp[e1] += 1
+            if mp[e1] > 1:
+                return e1
+            
+            mp[e2] += 1
+            if mp[e2] > 1:
+                return e2
+        
+        return -1
+            
